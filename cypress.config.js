2a680b2c306969511e312e8ceab625737d2eaeff
SevcanEgretli <sevcan.egretli@adjust.com>
2025-05-20 21:23:16 +0300

    report update

--- cypress.config.js
+++ cypress.config.js
@@ -5,16 +5,17 @@ module.exports = defineConfig({
     baseUrl: 'https://demoqa.com',
     specPattern: 'cypress/e2e/**/*.cy.js',
     supportFile: 'cypress/support/e2e.js',
-    reporter: 'mochawesome',
-    reporterOptions: {
-      reportDir: 'cypress/reports',
-      overwrite: false,
-      html: true,
-      json: false
-    },
     viewportWidth: 1280,
     viewportHeight: 720,
     video: false,
     screenshotOnRunFailure: true,
+    pageLoadTimeout: 120000
   },
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: false,
+    json: true
+  }
 });
